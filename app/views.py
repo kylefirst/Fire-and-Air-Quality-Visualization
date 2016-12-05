9fa4a50bea758035904329a7ee5102cf50f74d71
Sean Brown <seanbrhn3@gmail.com>
2016-12-04 19:33:31 -0500

    it finally work!

--- app/views.py
+++ app/views.py
@@ -4,5 +4,9 @@ from app import app
 
 @app.route('/')
 @app.route('/index')
+
 def index():
     return render_template('index.html')
+@app.route('/firstHTML')
+def firstHTML():
+    return render_template('firstHTML.html')
